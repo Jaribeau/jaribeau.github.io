ed0dacf1cf4b15b8e2ccb1873646f9773f845637
Jared Baribeau <jared.baribeau@smartex.ai>
2026-05-03 20:14:23 +0200

    Added Tiny Apps section and scroll to top button.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(function() {
     $('.pop').on('click', function() {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');   
-    });		
+        $('#imagemodal').modal('show');
+    });
+
+    var $backToTop = $('.back-to-top');
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 400) {
+            $backToTop.addClass('visible');
+        } else {
+            $backToTop.removeClass('visible');
+        }
+    });
 });
